a972a3494f16ef66ede23f7d48d7eb0f62b7b869
tokodev <pablo@asdloop.com>
2023-10-23 13:48:51 +0200

    Delete title for History section

--- packages/nextjs/components/screens/History.tsx
+++ packages/nextjs/components/screens/History.tsx
@@ -6,7 +6,6 @@ import { GemHistory } from "~~/components/screens/GemHistory";
 export const History = () => {
   return (
     <div className="flex flex-col gap-2">
-      <h2 className="font-bold mb-0 mt-2">Gem History</h2>
       <div className="mt-0 w-full">
         <GemHistory />
       </div>
