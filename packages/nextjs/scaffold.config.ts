a729bd2aa2856c366087f2f7a9a15097b6662567
tokodev <pablo@asdloop.com>
2023-10-25 15:12:40 +0200

    Tweak liveURL to the right localhost url

--- packages/nextjs/scaffold.config.ts
+++ packages/nextjs/scaffold.config.ts
@@ -14,7 +14,7 @@ export type ScaffoldConfig = {
 const scaffoldConfig = {
   // The network where your DApp lives in
   targetNetwork: chains.hardhat,
-  liveUrl: process.env.NEXT_PUBLIC_LIVE_URL || "http://localhost:3000/example-ui",
+  liveUrl: process.env.NEXT_PUBLIC_LIVE_URL || "http://localhost:3000/wallet",
 
   // The interval at which your front-end polls the RPC servers for new data
   // it has no effect on the local network
