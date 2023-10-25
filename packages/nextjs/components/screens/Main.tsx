6c96e939fa82a1af7f3eac68ba9586e125bf4208
tokodev <pablo@asdloop.com>
2023-10-25 15:14:34 +0200

    Add mint button to test the wallet with Gems

--- packages/nextjs/components/screens/Main.tsx
+++ packages/nextjs/components/screens/Main.tsx
@@ -1,5 +1,7 @@
 import QRCode from "react-qr-code";
+import { parseEther } from "viem";
 import { useAccount } from "wagmi";
+import { useScaffoldContractWrite } from "~~/hooks/scaffold-eth";
 // import { Address } from "~~/components/scaffold-eth";
 import scaffoldConfig from "~~/scaffold.config";
 
@@ -8,6 +10,21 @@ import scaffoldConfig from "~~/scaffold.config";
  */
 export const Main = () => {
   const { address } = useAccount();
+
+  const { writeAsync: mint, isMining } = useScaffoldContractWrite({
+    contractName: "EventGems",
+    functionName: "mint",
+    args: [address, parseEther("10")],
+  });
+
+  const handleMint = async () => {
+    try {
+      // Call the mint function with the desired amount (10 gems)
+      await mint();
+    } catch (error) {
+      console.error("Minting failed:", error);
+    }
+  };
 
   return (
     <div className="flex flex-col items-center justify-center p-8 pt-0">
@@ -22,6 +39,9 @@ export const Main = () => {
           viewBox={`0 0 256 256`}
         />
       )}
+      <button onClick={handleMint} className={`btn btn-primary w-full mt-8 ${isMining ? "loading" : ""}`}>
+        Mint 10 💎 Gems
+      </button>
     </div>
   );
 };
